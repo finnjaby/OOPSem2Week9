e02d3b82943a026018f5126a0b684f0140318e77
FinnjaBayer <g00470359@atu.ie>
2026-03-18 16:13:06 +0000

    created new project

---
+++ src/main/java/ie/atu/oopsem2week9/OopSem2Week9Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oopsem2week9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Week9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopSem2Week9Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oopsem2week9/OopSem2Week9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oopsem2week9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSem2Week9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
